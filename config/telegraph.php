ebd1b7441dfe204739e20610903dac8edd906596
GitHub Action <action@github.com>
2024-03-21 21:07:49 +0000

    🧹 Fixed code-style

--- config/telegraph.php
+++ config/telegraph.php
@@ -30,9 +30,7 @@ return [
          */
         'handler' => DefStudio\Telegraph\Handlers\EmptyWebhookHandler::class,
 
-        /*
-         * Middleware to be applied to the webhook route
-         */
+        // Middleware to be applied to the webhook route
         'middleware' => [],
 
         /*
@@ -56,21 +54,14 @@ return [
         'debug' => false,
     ],
 
-
     'security' => [
-        /*
-         * if enabled, allows callback queries from unregistered chats
-         */
+        // if enabled, allows callback queries from unregistered chats
         'allow_callback_queries_from_unknown_chats' => true,
 
-        /*
-         * if enabled, allows messages and commands from unregistered chats
-         */
+        // if enabled, allows messages and commands from unregistered chats
         'allow_messages_from_unknown_chats' => true,
 
-        /*
-         * if enabled, store unknown chats as new TelegraphChat models
-         */
+        // if enabled, store unknown chats as new TelegraphChat models
         'store_unknown_chats_in_db' => true,
     ],
 
@@ -82,23 +73,21 @@ return [
      * Chat model must be or extend `DefStudio\Telegraph\Models\TelegraphChat::class`
      */
     'models' => [
-        'bot' => DefStudio\Telegraph\Models\TelegraphBot::class,
+        'bot'  => DefStudio\Telegraph\Models\TelegraphBot::class,
         'chat' => DefStudio\Telegraph\Models\TelegraphChat::class,
     ],
 
     'storage' => [
-        /**
-         * Default storage driver to be used for Telegraph data
-         */
+        // Default storage driver to be used for Telegraph data
         'default' => 'cache',
 
         'stores' => [
             'file' => [
-                /**
+                /*
                  * Telegraph cache driver to be used, must implement
                  * DefStudio\Telegraph\Contracts\StorageDriver contract
                  */
-                'driver' => \DefStudio\Telegraph\Storage\FileStorageDriver::class,
+                'driver' => DefStudio\Telegraph\Storage\FileStorageDriver::class,
 
                 /*
                  * Laravel Storage disk to use. See /config/filesystems/disks for available disks
@@ -106,17 +95,15 @@ return [
                  */
                 'disk' => 'local',
 
-                /**
-                 * Folder inside filesystem to be used as root for Telegraph storage
-                 */
+                // Folder inside filesystem to be used as root for Telegraph storage
                 'root' => 'telegraph',
             ],
             'cache' => [
-                /**
+                /*
                  * Telegraph cache driver to be used, must implement
                  * DefStudio\Telegraph\Contracts\StorageDriver contract
                  */
-                'driver' => \DefStudio\Telegraph\Storage\CacheStorageDriver::class,
+                'driver' => DefStudio\Telegraph\Storage\CacheStorageDriver::class,
 
                 /*
                  * Laravel Cache store to use. See /config/cache/stores for available stores
@@ -124,29 +111,27 @@ return [
                  */
                 'store' => null,
 
-                /*
-                 * Prefix to be prepended to cache keys
-                 */
+                // Prefix to be prepended to cache keys
                 'key_prefix' => 'tgph',
             ],
         ],
     ],
 
-    /**
+    /*
      * Attachment validation rules, Telegram bot API defaults are set
      * can be changed to match higher limits when using a local bot
      * API server (ref. https://core.telegram.org/bots/api#using-a-local-bot-api-server)
      */
     'attachments' => [
         'thumbnail' => [
-            'max_size_kb' => 200,
+            'max_size_kb'   => 200,
             'max_height_px' => 320,
-            'max_width_px' => 320,
-            'allowed_ext' => ['jpg'],
+            'max_width_px'  => 320,
+            'allowed_ext'   => ['jpg'],
         ],
         'photo' => [
-            'max_size_mb' => 10,
-            'max_ratio' => 20,
+            'max_size_mb'         => 10,
+            'max_ratio'           => 20,
             'height_width_sum_px' => 10000,
         ],
         'animation' => [
