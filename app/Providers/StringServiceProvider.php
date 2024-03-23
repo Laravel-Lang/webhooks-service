7feb8154ac8ad138f43680cf8f18c15c1c7ad86e
Andrey Helldar <helldar@dragon-code.pro>
2024-03-23 23:05:55 +0300

    Fixed determining the number of lines

--- app/Providers/StringServiceProvider.php
+++ app/Providers/StringServiceProvider.php
@@ -13,13 +13,12 @@ class StringServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Stringable::macro('limitRows', function (int $limit) {
-            $result = $this
-                ->explode(PHP_EOL)
+            $rows = $this->explode(PHP_EOL);
+
+            $result = $rows
                 ->take($limit)
-                ->when(
-                    fn (Collection $items) => $limit < $items->count(),
-                    fn (Collection $items) => $items->push('...')
-                )->implode(PHP_EOL);
+                ->when($rows > $limit, fn (Collection $items) => $items->push('...'))
+                ->implode(PHP_EOL);
 
             return new Stringable($result);
         });
