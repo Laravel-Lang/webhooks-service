9f26628f1eb361c7c210d37bf75d5e2f9427d973
Andrey Helldar <helldar@dragon-code.pro>
2024-04-01 01:19:16 +0300

    Added action for resetting all webhooks from repositories

---
+++ actions/2024_04_01_010149_remove_unnecessary_webhooks.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use DragonCode\LaravelActions\Action;
+use GrahamCampbell\GitHub\GitHubManager;
+
+return new class extends Action {
+    protected bool $before = false;
+
+    protected string $organization = 'Laravel-Lang';
+
+    public function __invoke(GitHubManager $github): void
+    {
+        foreach ($this->repositories($github) as $repository) {
+            foreach ($this->webhooks($github, $repository['name']) as $webhook) {
+                $this->remove($github, $repository['name'], $webhook['id']);
+            }
+        }
+    }
+
+    protected function remove(GitHubManager $github, string $repository, int $id): void
+    {
+        $github->repository()->hooks()->remove($this->organization, $repository, $id);
+    }
+
+    protected function repositories(GitHubManager $github): array
+    {
+        return $github->organization()->repositories($this->organization);
+    }
+
+    protected function webhooks(GitHubManager $github, string $repository): array
+    {
+        return $github->repository()->hooks()->all($this->organization, $repository);
+    }
+};
