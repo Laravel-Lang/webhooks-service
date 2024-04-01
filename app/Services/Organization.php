4756bd5524f3df1a6a9a5e3dbc0c6afa69f75121
Andrey Helldar <helldar@dragon-code.pro>
2024-04-01 04:17:05 +0300

    Fixed processing of organization repositories taking into account pagination

--- app/Services/Organization.php
+++ app/Services/Organization.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Github\ResultPager;
 use GrahamCampbell\GitHub\GitHubManager;
 
 class Organization
 {
     public function __construct(
-        protected GitHubManager $github
+        protected GitHubManager $github,
+        protected ResultPager $paginator
     ) {}
 
     public function repositories(string $organization): array
     {
-        return $this->github->repositories()->org($organization);
+        return $this->paginator->fetchAll($this->github->repositories(), 'org', [$organization]);
     }
 }
