61aeb6a1182837029b5091ea7a8203e2039b7f91
Andrey Helldar <helldar@dragon-code.pro>
2024-12-20 12:46:36 +0300

    Added operation for resending releases

---
+++ operations/2024_12_20_094555_resend_releases.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use DragonCode\LaravelDeployOperations\Operation;
+
+return new class extends Operation {
+    protected bool $before = false;
+
+    public function __invoke(): void
+    {
+        $this->artisan('queue:retry', [
+            '--queue' => 'releases',
+        ]);
+    }
+};
