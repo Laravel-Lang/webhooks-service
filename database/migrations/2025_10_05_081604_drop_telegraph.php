74b280313803fe328ffc4ebf7b7c23eadbf3d4b0
Andrey Helldar <helldar@dragon-code.pro>
2025-10-05 11:23:46 +0300

    Dependent objects still exist

--- database/migrations/2025_10_05_081604_drop_telegraph.php
+++ database/migrations/2025_10_05_081604_drop_telegraph.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::dropIfExists('telegraph_bots');
         Schema::dropIfExists('telegraph_chats');
+        Schema::dropIfExists('telegraph_bots');
     }
 };
